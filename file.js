33467ad3c418f3d218737220768eeddafbc1ecd9
PranabZz <pranabkca321@gmail.com>
2024-07-17 07:42:49 +0545

    File.js added

---
+++ file.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+class Person{
+  constructor(name,age){
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const Pranab = new Person('Pranab', 12);
+
+console.log(Pranab);
